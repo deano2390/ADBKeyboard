1eb7081f29114b8843cf87bb36fe09169f00875c
deano2390 <dw86@kent.ac.uk>
2014-09-26 15:05:52 +0100

    initial commit

---
+++ AdbKeyboard/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace AdbKeyboard
+{
+    partial class AdbKeyboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdbKeyboard));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSendMacro1 = new System.Windows.Forms.Button();
+            this.tbMacro1 = new System.Windows.Forms.TextBox();
+            this.pnlMacros = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMainHeader = new System.Windows.Forms.Label();
+            this.tbMacro2 = new System.Windows.Forms.TextBox();
+            this.btnSendMacro2 = new System.Windows.Forms.Button();
+            this.tbMacro3 = new System.Windows.Forms.TextBox();
+            this.btnSendMacro3 = new System.Windows.Forms.Button();
+            this.tbMacro4 = new System.Windows.Forms.TextBox();
+            this.btnSendMacro4 = new System.Windows.Forms.Button();
+            this.tbMacro5 = new System.Windows.Forms.TextBox();
+            this.btnSendMacro5 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkHelp = new System.Windows.Forms.LinkLabel();
+            this.pnlMacros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 42);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(330, 87);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Keyboard";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(12, 135);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(330, 91);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop Keyboard";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSendMacro1
+            // 
+            this.btnSendMacro1.Location = new System.Drawing.Point(276, 32);
+            this.btnSendMacro1.Name = "btnSendMacro1";
+            this.btnSendMacro1.Size = new System.Drawing.Size(67, 64);
+            this.btnSendMacro1.TabIndex = 4;
+            this.btnSendMacro1.Text = "Send";
+            this.btnSendMacro1.UseVisualStyleBackColor = true;
+            this.btnSendMacro1.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbMacro1
+            // 
+            this.tbMacro1.Location = new System.Drawing.Point(13, 32);
+            this.tbMacro1.Multiline = true;
+            this.tbMacro1.Name = "tbMacro1";
+            this.tbMacro1.Size = new System.Drawing.Size(253, 64);
+            this.tbMacro1.TabIndex = 5;
+            // 
+            // pnlMacros
+            // 
+            this.pnlMacros.Controls.Add(this.tbMacro5);
+            this.pnlMacros.Controls.Add(this.btnSendMacro5);
+            this.pnlMacros.Controls.Add(this.tbMacro4);
+            this.pnlMacros.Controls.Add(this.btnSendMacro4);
+            this.pnlMacros.Controls.Add(this.tbMacro3);
+            this.pnlMacros.Controls.Add(this.btnSendMacro3);
+            this.pnlMacros.Controls.Add(this.tbMacro2);
+            this.pnlMacros.Controls.Add(this.btnSendMacro2);
+            this.pnlMacros.Controls.Add(this.label1);
+            this.pnlMacros.Controls.Add(this.tbMacro1);
+            this.pnlMacros.Controls.Add(this.btnSendMacro1);
+            this.pnlMacros.Enabled = false;
+            this.pnlMacros.Location = new System.Drawing.Point(-1, 232);
+            this.pnlMacros.Name = "pnlMacros";
+            this.pnlMacros.Size = new System.Drawing.Size(358, 392);
+            this.pnlMacros.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Macros";
+            // 
+            // lblMainHeader
+            // 
+            this.lblMainHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMainHeader.AutoSize = true;
+            this.lblMainHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainHeader.ForeColor = System.Drawing.Color.Red;
+            this.lblMainHeader.Location = new System.Drawing.Point(13, 9);
+            this.lblMainHeader.Name = "lblMainHeader";
+            this.lblMainHeader.Size = new System.Drawing.Size(0, 25);
+            this.lblMainHeader.TabIndex = 7;
+            this.lblMainHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbMacro2
+            // 
+            this.tbMacro2.Location = new System.Drawing.Point(13, 102);
+            this.tbMacro2.Multiline = true;
+            this.tbMacro2.Name = "tbMacro2";
+            this.tbMacro2.Size = new System.Drawing.Size(253, 64);
+            this.tbMacro2.TabIndex = 8;
+            // 
+            // btnSendMacro2
+            // 
+            this.btnSendMacro2.Location = new System.Drawing.Point(276, 102);
+            this.btnSendMacro2.Name = "btnSendMacro2";
+            this.btnSendMacro2.Size = new System.Drawing.Size(67, 64);
+            this.btnSendMacro2.TabIndex = 7;
+            this.btnSendMacro2.Text = "Send";
+            this.btnSendMacro2.UseVisualStyleBackColor = true;
+            this.btnSendMacro2.Click += new System.EventHandler(this.btnSendMacro2_Click);
+            // 
+            // tbMacro3
+            // 
+            this.tbMacro3.Location = new System.Drawing.Point(13, 172);
+            this.tbMacro3.Multiline = true;
+            this.tbMacro3.Name = "tbMacro3";
+            this.tbMacro3.Size = new System.Drawing.Size(253, 64);
+            this.tbMacro3.TabIndex = 10;
+            // 
+            // btnSendMacro3
+            // 
+            this.btnSendMacro3.Location = new System.Drawing.Point(276, 172);
+            this.btnSendMacro3.Name = "btnSendMacro3";
+            this.btnSendMacro3.Size = new System.Drawing.Size(67, 64);
+            this.btnSendMacro3.TabIndex = 9;
+            this.btnSendMacro3.Text = "Send";
+            this.btnSendMacro3.UseVisualStyleBackColor = true;
+            this.btnSendMacro3.Click += new System.EventHandler(this.btnSendMacro3_Click);
+            // 
+            // tbMacro4
+            // 
+            this.tbMacro4.Location = new System.Drawing.Point(13, 242);
+            this.tbMacro4.Multiline = true;
+            this.tbMacro4.Name = "tbMacro4";
+            this.tbMacro4.Size = new System.Drawing.Size(253, 64);
+            this.tbMacro4.TabIndex = 12;
+            // 
+            // btnSendMacro4
+            // 
+            this.btnSendMacro4.Location = new System.Drawing.Point(276, 242);
+            this.btnSendMacro4.Name = "btnSendMacro4";
+            this.btnSendMacro4.Size = new System.Drawing.Size(67, 64);
+            this.btnSendMacro4.TabIndex = 11;
+            this.btnSendMacro4.Text = "Send";
+            this.btnSendMacro4.UseVisualStyleBackColor = true;
+            this.btnSendMacro4.Click += new System.EventHandler(this.btnSendMacro4_Click);
+            // 
+            // tbMacro5
+            // 
+            this.tbMacro5.Location = new System.Drawing.Point(14, 312);
+            this.tbMacro5.Multiline = true;
+            this.tbMacro5.Name = "tbMacro5";
+            this.tbMacro5.Size = new System.Drawing.Size(253, 64);
+            this.tbMacro5.TabIndex = 14;
+            // 
+            // btnSendMacro5
+            // 
+            this.btnSendMacro5.Location = new System.Drawing.Point(277, 312);
+            this.btnSendMacro5.Name = "btnSendMacro5";
+            this.btnSendMacro5.Size = new System.Drawing.Size(67, 64);
+            this.btnSendMacro5.TabIndex = 13;
+            this.btnSendMacro5.Text = "Send";
+            this.btnSendMacro5.UseVisualStyleBackColor = true;
+            this.btnSendMacro5.Click += new System.EventHandler(this.btnSendMacro5_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 632);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(123, 17);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "© Dean Wild 2014";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkHelp
+            // 
+            this.linkHelp.AutoSize = true;
+            this.linkHelp.Location = new System.Drawing.Point(273, 635);
+            this.linkHelp.Name = "linkHelp";
+            this.linkHelp.Size = new System.Drawing.Size(48, 17);
+            this.linkHelp.TabIndex = 9;
+            this.linkHelp.TabStop = true;
+            this.linkHelp.Text = "Help?!";
+            this.linkHelp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkHelp_LinkClicked);
+            // 
+            // AdbKeyboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 661);
+            this.Controls.Add(this.linkHelp);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblMainHeader);
+            this.Controls.Add(this.pnlMacros);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "AdbKeyboard";
+            this.Text = "AdbKeyboard";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AdbKeyboard_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AdbKeyboard_KeyPress);
+            this.pnlMacros.ResumeLayout(false);
+            this.pnlMacros.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSendMacro1;
+        private System.Windows.Forms.TextBox tbMacro1;
+        private System.Windows.Forms.Panel pnlMacros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMainHeader;
+        private System.Windows.Forms.TextBox tbMacro5;
+        private System.Windows.Forms.Button btnSendMacro5;
+        private System.Windows.Forms.TextBox tbMacro4;
+        private System.Windows.Forms.Button btnSendMacro4;
+        private System.Windows.Forms.TextBox tbMacro3;
+        private System.Windows.Forms.Button btnSendMacro3;
+        private System.Windows.Forms.TextBox tbMacro2;
+        private System.Windows.Forms.Button btnSendMacro2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkHelp;
+    }
+}
+
---
+++ AdbKeyboard/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Timer = System.Windows.Forms.Timer;
+
+namespace AdbKeyboard
+{
+    public partial class AdbKeyboard : Form
+    {
+
+        private string adbPath = @"D:\ADT\adt-bundle-windows-x86_64-20130729\sdk\platform-tools\adb";
+
+        public AdbKeyboard()
+        {
+            InitializeComponent();
+            LoadMacros();
+        }
+
+        private Process process;
+
+        private string history = string.Empty;
+        private string textBuffer = string.Empty;
+        private string keyEventBuffer = string.Empty;
+
+        private Timer timer = new Timer();
+        private int milliSeconds = 0;
+
+
+        private void Enable()
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            pnlMacros.Enabled = true;
+            lblMainHeader.Text = "ADB Keyboard Running";
+            lblMainHeader.Focus();
+        }
+
+        private void Disable()
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            pnlMacros.Enabled = false;
+            lblMainHeader.Text = "";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            process = new Process {StartInfo = startInfo};
+            process.Start();
+
+            process.StandardInput.WriteLine("adb shell");
+
+            Enable();
+
+            timer.Tick += timer_Tick;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            process.Close();
+            process = null;
+            Disable();
+            timer.Tick -= timer_Tick;
+        }
+
+        private void AdbKeyboard_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (pnlMacros.ContainsFocus) return;
+
+            if (process != null)
+            {
+
+                ResetTimer();
+
+                switch (e.KeyChar)
+                {
+                    case (char) Keys.Space:
+                        flushTextBuffer();
+                        keyEventBuffer += "62 "; // spacebar
+                        break;
+                    case (char) Keys.Back:
+                        flushTextBuffer();
+                        keyEventBuffer += "67 "; // backspace
+                        break;
+
+                    case (char)Keys.Escape:
+                        flushTextBuffer();
+                        keyEventBuffer += "4 "; // back button
+                        break;
+
+                    case (char)Keys.Enter:
+                        flushTextBuffer();
+                        keyEventBuffer += "66 "; // enter
+                        break;
+
+                    default:
+                        flushKeyBuffer();
+                        textBuffer += e.KeyChar;
+                        break;
+                }
+            }
+        }
+        
+        private void AdbKeyboard_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (pnlMacros.ContainsFocus) return;
+
+            bool keyEventWasConsumed = true;
+            string newBuffer = string.Empty;
+
+            switch (e.KeyCode)
+            {
+                case Keys.F1:
+                    newBuffer += "3 "; // home button
+                    break;
+
+                    case Keys.F2:
+                    newBuffer += "26 "; // power button
+                    break;
+
+                    case Keys.End:
+                    newBuffer += "123 "; // end
+                    break;
+
+                    case Keys.Home:
+                    newBuffer += "122 "; // end
+                    break;
+
+                    case Keys.Delete:
+                    newBuffer += "112 "; // delete
+                    break;
+
+                default:
+                    keyEventWasConsumed = false;
+                    break;
+            }
+
+            if (keyEventWasConsumed)
+            {
+                ResetTimer();
+                flushTextBuffer();
+                keyEventBuffer += newBuffer;
+            }
+               
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+
+            if (keyData == Keys.Tab && !pnlMacros.ContainsFocus)
+            {
+                ResetTimer();
+                flushTextBuffer();
+                keyEventBuffer += "187 ";
+                return true;
+            }
+            else
+            {
+                return base.ProcessCmdKey(ref msg, keyData);   
+            }
+        }
+
+        private void ResetTimer()
+        {
+            if (!timer.Enabled) timer.Start();
+            milliSeconds = 0;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            milliSeconds++;
+            if (milliSeconds > 2 || keyEventBuffer.Length > 5 || textBuffer.Length > 10)
+            {
+                flushTextBuffer();
+                flushKeyBuffer();
+            }
+        }
+
+        private void flushTextBuffer()
+        {
+            if (textBuffer.Length > 0)
+            {
+                timer.Stop();
+                milliSeconds = 0;
+
+                process.StandardInput.WriteLine(@"input text " + textBuffer);
+                history = textBuffer + "\r\n" + history;
+
+                textBuffer = string.Empty;
+            }
+        }
+
+        private void flushKeyBuffer()
+        {
+            if (keyEventBuffer.Length > 0)
+            {
+                timer.Stop();
+                milliSeconds = 0;
+
+                if (keyEventBuffer.Length > 0)
+                {
+                    process.StandardInput.WriteLine(@"input keyevent " + keyEventBuffer);
+                    history = keyEventBuffer + "\r\n" + history;
+                }
+
+                keyEventBuffer = string.Empty;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMacro(tbMacro1);
+        }
+
+        private void btnSendMacro2_Click(object sender, EventArgs e)
+        {
+            SendMacro(tbMacro2);
+        }
+
+        private void btnSendMacro3_Click(object sender, EventArgs e)
+        {
+            SendMacro(tbMacro3);
+        }
+
+        private void btnSendMacro4_Click(object sender, EventArgs e)
+        {
+            SendMacro(tbMacro4);
+        }
+
+        private void btnSendMacro5_Click(object sender, EventArgs e)
+        {
+            SendMacro(tbMacro5);
+        }
+
+        private void SendMacro(TextBox textBox)
+        {
+            if (process != null)
+            {
+                if (textBox.Text.Length > 0)
+                {
+                    process.StandardInput.WriteLine(@"input text " + textBox.Text);
+                }
+            }
+
+            Properties.Settings.Default[textBox.Name] = textBox.Text;
+            Properties.Settings.Default.Save(); // Saves settings in application configuration file
+
+            lblMainHeader.Focus();
+        }
+
+        private void LoadMacros()
+        {
+            foreach(Control control in pnlMacros.Controls)
+            {
+                if (control is TextBox)
+                {
+                    TextBox textBox = (TextBox) control;
+                    textBox.Text = Properties.Settings.Default[textBox.Name].ToString();
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/deano2390");
+        }
+
+        private void linkHelp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("This program is a tool for making repetitive text entry a little less painful when testing Android apps. Run this program and click the start button, while the app is focused it will forward your keyboard input to any Android device connected via ADB. Use the macro textboxes to save your commonly used text snippets.", "Help");
+        }
+    }
+}
