d1d22d287ce51cc4e3537bb399140fd772a97a9b
deano2390 <dw86@kent.ac.uk>
2014-09-29 12:41:54 +0100

    remove unused refs

--- AdbKeyboard/Form1.cs
+++ AdbKeyboard/Form1.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 using Timer = System.Windows.Forms.Timer;
 
 namespace AdbKeyboard
